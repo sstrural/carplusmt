ef2baf79cf99d169db10f108c13044e79c1dd523
SEMA-MT <sema.mt@gov.br>
2026-06-25 20:13:57 -0400

    feat: implementar PWA e branding customizável

---
+++ public/sw.js
@@ -0,0 +1,50 @@
+const CACHE_NAME = 'car-mt-analisador-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './vendor/shpwrite.js',
+  './vendor/jspdf.umd.min.js'
+];
+
+// Instalar o Service Worker e cachear os arquivos
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+// Ativar o Service Worker e limpar caches antigos
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.filter(cacheName => cacheName !== CACHE_NAME)
+          .map(cacheName => caches.delete(cacheName))
+      );
+    })
+  );
+});
+
+// Fetch: tentar da rede, se falhar usar cache
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request).then(response => {
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+          const responseToCache = response.clone();
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+          return response;
+        });
+      })
+  );
+});
